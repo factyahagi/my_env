11bb072c08ed146f71646f97ca3f1aaa599d4b42
tomoaki yahagi <yahagi@fact-real.com>
2013-05-13 13:52:29 +0900

    Rename  startup-vim-ruby.sh -> vim-bundles.sh, and Updated!

--- scripts/startup-vim-ruby.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir -r ~/.vim/bundle
-git clone git://github.com/vim-ruby/vim-ruby.git ~/.vim/bundle/vim-ruby
-
---
+++ scripts/vim-bundles.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+mkdir -r ~/.vim/bundle
+
+# Ruby & Rails
+git clone git://github.com/vim-ruby/vim-ruby.git ~/.vim/bundle/vim-ruby
+git clone git://github.com/tpope/vim-rails.git
+git clone git://github.com/tpope/vim-bundler.git
